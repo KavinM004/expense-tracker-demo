8896c4826b2172ac1b483f3634e0199a41c056ea
KavinM <karthickkavin004@gmail.com>
2025-02-20 00:07:00 +0530

    done

--- client/expense-tracker/src/pages/Dashboard.jsx
+++ client/expense-tracker/src/pages/Dashboard.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Navbar from "../components/Navbar.jsx";
-import Content from "../components/Content.jsx";
 
 const Dashboard = () => {
   return (
     <>
         <Navbar />
-        <Content />
     </>
   );
 };
